618e52b374591d877142beed7b97c820debcaa81
Vasu Ratanpara <vasuratanpara@gmail.com>
2020-05-18 23:23:07 -0400

    Final Push

--- Assignment 1/client/client.py
+++ Assignment 1/client/client.py
@@ -1,44 +1,51 @@
+import re
 import sys
 import json
 import socket
-from collections import OrderedDict
 
 # Global Vars
 HOST, PORT, MAX_SIZE = "localhost", 9999, 1024
 
+# ANSI Escape Sequence Colors
+GreenColor, RedColor, YellowColor, LightPurpleColor, PurpleColor, CyanColor, LightGrayColor, BlackColor, ResetColor ="\033[92m", "\033[91m", "\033[93m", "\033[94m", "\033[95m", "\033[96m","\033[97m","\033[98m","\033[00m"
+
 # Menu for Client
 def menu():
-    print("Python DB Menu\n1. Find customer\n2. Add customer\n3. Delete customer\n4. Update customer age\n5. Update customer address\n6. Update customer phone\n7. Print report\n8. Exit\n")
+    print("\nPython DB Menu\n1. Find customer\n2. Add customer\n3. Delete customer\n4. Update customer age\n5. Update customer address\n6. Update customer phone\n7. Print report\n8. Exit\n")
     return input("Select:").strip()   
     
 # 1. Find customer
 def find_customer():
     # Get customer name
-    customer_name = input("Enter a customer's name :").strip()
+    customer_name = input("Enter a customer's name :").strip().lower()
     # Validate customer_name
-    if not customer_name or is_int(customer_name):
-        print_error("Customer's name cannot be empty and/or Integer")
-        return
+    if not is_valid_name(customer_name):return
 
     # Preparing Data to send   
     data = {"action":"find_customer","name":customer_name}
     # send customer_name to server
     received = communicate(json.dumps(data))
     # Receive data from the server
-    print_server_msg(received)
+    print_server_msg(received,True)
 
 # 2. Add customer
 def add_customer():    
     # Get customer details
-    customer_name    = input("Enter a new customer's name:").strip()
+    customer_name    = input("Enter a new customer's name:").strip().lower()
     # Validate customer_name
-    if not customer_name or is_int(customer_name):
-        print_error("Customer's name cannot be empty and/or Integer")
-        return
+    if not is_valid_name(customer_name):return
 
     customer_age     = input("Enter a new customer's age:").strip()
+    # Validate customer_age
+    if not is_valid_age(customer_age):return
+
     customer_address = input("Enter a new customer's address:").strip()
+    # Validate customer_address
+    if not is_valid_address(customer_address):return
+
     customer_phone   = input("Enter a new customer's phone:").strip()
+    # Validate customer_phone
+    if not is_valid_phone(customer_phone):return
 
     # Create user's dictionary
     data = {"action":"add_customer","name":customer_name,"age":customer_age,"address":customer_address,"phone":customer_phone}
@@ -46,16 +53,16 @@ def add_customer():
     received = communicate(json.dumps(data))
     # Receive data from the server
     print_server_msg(received)
+    if json.loads(received)['status'] == "true":
+        print_report()
 
 
 # 3. Delete customer
 def delete_customer():
     # Get customer name
-    customer_name = input("Enter a customer's name :").strip()
+    customer_name = input("Enter a customer's name :").strip().lower()
     # Validate customer_name
-    if not customer_name or is_int(customer_name):
-        print_error("Customer's name cannot be empty and/or Integer")
-        return
+    if not is_valid_name(customer_name):return
 
     # Preparing Data to send   
     data = {"action":"delete_customer","name":customer_name}
@@ -63,22 +70,20 @@ def delete_customer():
     received = communicate(json.dumps(data))
     # Receive data from the server
     print_server_msg(received)
+    if json.loads(received)['status'] == "true":
+        print_report()
 
 # 4. Update customer age
 def update_customer_age():
     # Get customer name
-    customer_name = input("Enter a customer's name :").strip()
+    customer_name = input("Enter a customer's name :").strip().lower()
     # Validate customer_name
-    if not customer_name or is_int(customer_name):
-        print_error("Customer's name cannot be empty and/or Integer")
-        return
+    if not is_valid_name(customer_name):return
 
     # Get new age of customer
     customer_age = input("Enter new age for {}:".format(customer_name)).strip()
     # Validate customer_age
-    if not customer_age or not is_int(customer_age):
-        print_error("Customer's age cannot be empty and/or String")
-        return
+    if not is_valid_age(customer_age):return
 
     # Preparing Data to send   
     data = {"action":"update_customer_age","name":customer_name,"age":customer_age}
@@ -86,21 +91,19 @@ def update_customer_age():
     received = communicate(json.dumps(data))
     # Receive data from the server
     print_server_msg(received)
+    if json.loads(received)['status'] == "true":
+        print_report()
 
 # 5. Update customer address
 def update_customer_address():
     #Get customer name
-    customer_name = input("Enter a customer's name :").strip()
+    customer_name = input("Enter a customer's name :").strip().lower()
     # Validate customer_name
-    if not customer_name or is_int(customer_name):
-        print_error("Customer's name cannot be empty and/or Integer")
-        return
+    if not is_valid_name(customer_name):return
     # Get new address of customer
     customer_address = input("Enter new address for {}:".format(customer_name)).strip()
     # Validate customer_address
-    if not customer_address or is_int(customer_address):
-        print_error("Customer's age cannot be empty and/or String")
-        return
+    if not is_valid_address(customer_address):return
 
     # Preparing Data to send   
     data = {"action":"update_customer_address","name":customer_name,"address":customer_address}
@@ -108,28 +111,27 @@ def update_customer_address():
     received = communicate(json.dumps(data))
     # Receive data from the server
     print_server_msg(received)
+    if json.loads(received)['status'] == "true":
+        print_report()
 
 # 6. Update customer phone
 def update_customer_phone():
     # Get customer name
-    customer_name = input("Enter a customer's name :").strip()
+    customer_name = input("Enter a customer's name :").strip().lower()
     # Validate customer_name
-    if not customer_name or is_int(customer_name):
-        print_error("Customer's name cannot be empty and/or Integer")
-        return
+    if not is_valid_name(customer_name):return
     # Get new phone of customer
     customer_phone = input("Enter new phone for {}:".format(customer_name)).strip()
     # Validate customer_phone
-    if not customer_phone:
-        print_error("Customer's phone cannot be empty")
-        return
-
+    if not is_valid_phone(customer_phone):return
     # Preparing Data to send   
     data = {"action":"update_customer_phone","name":customer_name,"phone":customer_phone}
     # send customer_name to server
     received = communicate(json.dumps(data))
     # Receive data from the server
     print_server_msg(received)
+    if json.loads(received)['status'] == "true":
+        print_report()
 
 # 7. Print report
 def print_report():
@@ -140,12 +142,11 @@ def print_report():
     # Converting into 'dict'
     customers = json.loads(received)    
     
-    print ("\n{:<10} {:<10} {:<20} {:<25}".format('Name','Age','Address','Phone'))
+    print (GreenColor+"\n{:<10} {:<10} {:<20} {:<25}".format('Name','Age','Address','Phone')+ResetColor)
     for customer in customers.values():
-        print("{:<10} {:<10} {:<20} {:<25}".format(customer['name'],customer['age'],customer['address'],customer['phone']))
-    print()
+        print(CyanColor+"{:<10} {:<10} {:<20} {:<25}".format(customer['name'],customer['age'],customer['address'],customer['phone'])+ResetColor)
 
-# Conver String to Bytes
+# Conver String to Bytes    
 def str_to_byte(string):
     return bytes(string + "\n", "utf-8")
 
@@ -157,13 +158,64 @@ def is_int(val):
         return False
     return True
 
+# Check if the name is valid
+def is_valid_name(customer_name):
+    if customer_name == "" or not customer_name.isalpha() or customer_name.isdigit():
+        print_error("Customer's name cannot be empty, Integer and Alphanumeric")
+        return False
+    else:
+        return True
+
+# Check if the age is valid
+def is_valid_age(customer_age):
+    if  customer_age != "" and not customer_age.isdigit():
+        print_error("Customer's age cannot be string")
+        return False
+    elif customer_age != "" and 0 <= int(customer_age) > 100:
+        print_error("Customer's age can only between 0 to 100")
+        return False
+    else:
+        return True
+
+# Check if the address number is valid
+def is_valid_address(customer_address):
+    if customer_address != "" and customer_address.isdigit():
+        print_error("Customer's address cannot be only Integer")
+        return False
+    else:
+        return True
+
+# Check if the phone number is valid
+def is_valid_phone(customer_phone):
+    charRe = re.compile(r'[^-+() 0-9]')
+    string = charRe.search(customer_phone)
+    if len(list(filter(lambda x: x.isdigit(),customer_phone))) > 11 :
+        print_error("Customer's phone can only contain 11 digits E.g. +1 (123) 456-7890")
+    if bool(string):
+        print_error("Customer's phone can only contain '+','-','(',')' and numbers E.g. +1 (123) 456-7890")
+    return not bool(string)
+
 # Print Error message 
 def print_error(message):
-    print("\n\033[91m Error >>> {}\033[00m\n".format(message))
+    print(RedColor+"\n Error >>> {}\n".format(message)+ResetColor)
 
 # Print Server message 
-def print_server_msg(message):
-    print("\n\033[92m Server Response >>> {}\033[00m\n".format(message))
+def print_server_msg(message,PrintUser=False):
+    
+    message = json.loads(message)
+    
+    # set color
+    if message['status'] == "false":
+        color = RedColor
+    else:
+        color = GreenColor
+
+    if PrintUser and message['status'] == "true":
+        user_details = (message['message']['name'],message['message']['age'],message['message']['address'],message['message']['phone'])
+        print(YellowColor+"\n Server Response >>>"+ResetColor+color+" {}\n".format(user_details)+ResetColor)
+    else:    
+        print(YellowColor+"\n Server Response >>>"+ResetColor+color+" {}\n".format(message['message'])+ResetColor)
+
 
 def communicate(data):
     try:
@@ -178,15 +230,12 @@ def communicate(data):
         return received
 
     except Exception as e:
-        print_error("Server is not running "+e)
-    finally:
-        # Closing Socket
-        sock.close()
+        print_error("Server is not running "+str(e))
+
 
 if __name__ == "__main__":
 
     while True:
-        # build Switch case here
         try:
             choice = int(menu())
             # 1. Find customer
--- Assignment 1/server/server.py
+++ Assignment 1/server/server.py
@@ -3,7 +3,7 @@ import json
 import socketserver
 
 # Global Vars
-HOST, PORT, MAX_SIZE = "localhost", 9999, 1024
+HOST, PORT, MAX_SIZE, DB_File = "localhost", 9999, 1024, "data.txt"
 
 class MyTCPHandler(socketserver.BaseRequestHandler):    
 
@@ -39,12 +39,12 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         customer_name = argument['name']
         # Find custmer into database
         result = memory_db.get(customer_name)
+
         if result is None:
-            self.request.sendall(self.str_to_byte("Customer not found"))
+            result = {"status":"false","message":"Customer not found"}
         else:
-            # Send back result to client
-            self.request.sendall(self.str_to_byte(json.dumps(result)))
-        return
+            result = {"status":"true","message":result}
+        self.request.sendall(self.str_to_byte(json.dumps(result)))
             
     # 2. Add customer
     def add_customer(self,argument):
@@ -55,12 +55,15 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         
         if result is None:
             # Store customer into memory_db
-            memory_db[argument['name']] = argument
-            self.request.sendall(self.str_to_byte("Customer has been added"))
+            name = argument['name']
+            argument['name']=name.capitalize()
+            memory_db[name] = argument
+            open(DB_File, "a").write("\n"+argument['name'].capitalize()+"|"+argument['age']+"|"+argument['address']+"|"+argument['phone'])
+            result = {"status":"true","message":"Customer has been added"}
         else:
             # Customer name already exist
-            self.request.sendall(self.str_to_byte("Customer already exists"))
-        return
+            result = {"status":"false","message":"Customer's name already exist"}
+        self.request.sendall(self.str_to_byte(json.dumps(result)))
 
     # 3. Delete customer
     def delete_customer(self,argument):
@@ -70,13 +73,14 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         result = memory_db.get(customer_name, "Customer does not exist")
         if type(result) == dict:
             # Delete customer
-            del memory_db[customer_name] 
-            # Send back result to client
-            self.request.sendall(self.str_to_byte("Customer has been deleted"))
-        else:
-            self.request.sendall(self.str_to_byte(result))
-        return
+            del memory_db[customer_name]
 
+            # Send back result to client
+            result = {"status":"true","message":"Customer has been deleted"}
+        else:
+            result = {"status":"false","message":result}
+        self.request.sendall(self.str_to_byte(json.dumps(result)))
+        
     # 4. Update customer age
     def update_customer_age(self,argument):
         
@@ -86,11 +90,11 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         if type(result) == dict:
             # Update customer's age
             memory_db[argument['name']]['age'] = argument['age']
-            self.request.sendall(self.str_to_byte("Customer's age has been updated"))
+            #self.update_in_file(argument['name'],"age",argument['age'])
+            result = {"status":"true","message":"Customer's age has been updated"}
         else:
-            self.request.sendall(self.str_to_byte(result))
-        return    
-
+            result = {"status":"false","message":result}
+        self.request.sendall(self.str_to_byte(json.dumps(result)))
 
     # 5. Update customer address
     def update_customer_address(self,argument):
@@ -101,24 +105,24 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         if type(result) == dict:
             # Update customer's address
             memory_db[argument['name']]['address'] = argument['address']
-            self.request.sendall(self.str_to_byte("Customer's address has been updated"))
+            #self.update_in_file(argument['name'],"address",argument['address'])
+            result = {"status":"true","message":"Customer's address has been updated"}
         else:
-            self.request.sendall(self.str_to_byte(result))
-        return
-
+            result = {"status":"false","message":result}
+        self.request.sendall(self.str_to_byte(json.dumps(result)))
+    
     # 6. Update customer phone
     def update_customer_phone(self,argument):
-
         # Find custmer into database
         result = memory_db.get(argument['name'], "Customer not found")
-        
         if type(result) == dict:
             # Update customer's phone
             memory_db[argument['name']]['phone'] = argument['phone']
-            self.request.sendall(self.str_to_byte("Customer's phone has been updated"))
+            #self.update_in_file(argument['name'],"phone",argument['phone'])
+            result = {"status":"true","message":"Customer's phone has been updated"}
         else:
-            self.request.sendall(self.str_to_byte(result))
-        return
+            result = {"status":"false","message":result}    
+        self.request.sendall(self.str_to_byte(json.dumps(result)))
 
     # 7. Print report
     def print_report(self):
@@ -134,19 +138,49 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
         return bytes(string + "\n", "utf-8")
 
     # Store in File
-    def store_in_file(self,data):
-        print(data)
-        
+    def update_in_file(self,name,filed,value):
+
+        orignalline = ""
+        newline     = ""
+        if filed in ['age','address','phone'] :
+
+            if filed == 'age' :
+                op=1
+            elif filed == 'address':
+                op=2
+            elif filed == 'phone':
+                op=3
+            else:
+                return False
+
+            # Read line by line from file
+            file = open(DB_File,'r')
+            filedata = file.readlines()
+            for line in filedata:
+                user = line.strip().split('|')
+                if name.capitalize() in line:
+                    orignalline = line
+                    newline     = line.replace(str(user[op]),str(value))
+            # Read whole file at once
+            file = open(DB_File,'r')
+            filedata = file.read()
+            filedata = filedata.replace(str(orignalline),str(newline))
+            # Write to file
+            file = open(DB_File,'w')
+            file.write(filedata)
+            return True
+        else:
+            return False
 
 if __name__ == "__main__":
 
     # Check if file is exist or not 
-    if not pathlib.Path('data.txt').exists():
-        #database_file = open("data.txt","w+")
-        print("Error >>> data.txt file doesn't exists in current directory")
+    if not pathlib.Path(DB_File).exists():
+        #database_file = open(DB_File,"w+")
+        print("Error >>> "+DB_File+" file doesn't exists in current directory")
         exit(0)
     else:
-        database_file = open("data.txt","r")
+        database_file = open(DB_File,"r")
     
     # Reading file and loading into the memory
     memory_db = {}
@@ -159,7 +193,7 @@ if __name__ == "__main__":
             user_db['age'] = user[1].strip()
             user_db['address'] = user[2].strip()
             user_db['phone'] = user[3].strip()
-            memory_db[user[0]] =  user_db
+            memory_db[user[0].lower()] =  user_db
         
     # Create the server, binding to localhost on port 9999
     with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
